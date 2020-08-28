1203ccee87e6e3fdf623437b48f5a18e3e065adc
hayden7913 <hayden321@gmail.com>
2020-08-28 12:54:58 -0700

    cleans up app.js for fresh start

--- app.js
+++ app.js
@@ -3,41 +3,18 @@ const mongoose = require('mongoose');
 const morgan = require('morgan');
 const bodyParser = require('body-parser');
 const cors = require('cors');
-const passport = require('passport');
-const cookieSession = require('cookie-session');
 
 require('dotenv').config();
-require('./src/core/config/passport.config');
-
-const productsRoutes = require('./src/products/products.routes');
-const ordersRoutes = require('./src/orders/orders.routes');
-const usersRoutes = require('./src/users/users.routes');
-const authRoutes = require('./src/auth/auth.routes');
-const establishmentRoutes = require('./src/establishment/establishment.routes');
-const tagsRoutes = require('./src/tags/tags.routes');
 
 const app = express();
 const http = require('http').createServer(app);
 
-const socket = require('./src/socket');
-const io = require('socket.io')(http);
-socket.loadConfig(io);
-exports.io = io;
-
-//apply general middlewares
+// apply general middlewares
 app.use(morgan('dev'));
 app.use(cors());
-app.use(bodyParser.json({ limit: '50mb' }));
-app.use(
-  cookieSession({
-    maxAge: 24 * 60 * 60,
-    keys: [process.env.SECRET_KEY]
-  })
-);
-app.use(passport.initialize());
-app.use(passport.session());
+app.use(bodyParser.json({ limit: '50mb' })); // TODO look up the limit arg
 
-//database initialization
+// database initialization
 mongoose.connect(process.env.DATABASE_URL, {
   useNewUrlParser: true,
   useUnifiedTopology: true
@@ -49,13 +26,12 @@ db.once('open', function() {
   console.log('Connected to a database');
 });
 
-//apply routes
-app.use('/products', productsRoutes);
-app.use('/orders', ordersRoutes);
-app.use('/users', usersRoutes);
-app.use('/auth', authRoutes);
-app.use('/establishments', establishmentRoutes);
-app.use('/tags', tagsRoutes);
+// apply routes
+
+app.get('/ping', (req, res) => {
+  res.json({message: 'pong'})
+})
+
 app.use((req, res, next) => {
   const error = new Error('Route not found');
   error.status = 404;
@@ -72,5 +48,5 @@ app.use((error, req, res, next) => {
 });
 
 http.listen(process.env.PORT, () =>
-  console.log(`Waitermaster magic is on port ${process.env.PORT}!`)
+  console.log(`Battery monitoring magic is on port ${process.env.PORT}!`)
 );
